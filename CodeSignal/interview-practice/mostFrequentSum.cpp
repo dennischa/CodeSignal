28620381a9b52b7cbd36182e6c07860138546e95
CHA KYUNG JUN <dennischa50@gmail.com>
2019-08-22 19:31:38 +0900

    Solved mostFrequentSum problem

---
+++ CodeSignal/interview-practice/mostFrequentSum.cpp
@@ -0,0 +1,66 @@
+﻿//
+//  mostFrequentSum.cpp
+//  CodeSignal
+//
+//  Created by 차경준 on 08/06/2019.
+//  Copyright © 2019 KyungJunCha. All rights reserved.
+//
+//  Problem URL : https://app.codesignal.com/interview-practice/task/9i2KvkNRzncy7Bzia
+//  Difficulty : Hard
+//  Note : Tree
+
+#include "interview-practice.h"
+
+//
+// Binary trees are already defined with this interface:
+// template<typename T>
+// struct Tree {
+//   Tree(const T &v) : value(v), left(nullptr), right(nullptr) {}
+//   T value;
+//   Tree *left;
+//   Tree *right;
+// };
+
+int travel(Tree<int>* t, map<int, int>& sums, int& count)
+{
+	if (t == nullptr)
+		return 0;
+
+	Tree<int>* left = t->left;
+	Tree<int>* right = t->right;
+	int value = t->value;
+
+	if (left != nullptr || right != nullptr)
+	{
+		value += travel(left, sums, count) + travel(right, sums, count);
+	}
+
+	if (sums.find(value) != sums.end())
+		sums[value]++;
+	else
+		sums[value] = 1;
+
+	count = max(sums[value], count);
+	return value;
+}
+
+std::vector<int> mostFrequentSum(Tree<int>* t) 
+{
+	int count = 0;
+	map<int, int> sums;
+
+	travel(t, sums, count);
+
+	vector<int> answer;
+
+	map<int, int>::iterator it = sums.begin();
+	for (; it != sums.end(); it++)
+	{
+		if (it->second == count)
+			answer.push_back(it->first);
+	}
+
+	sort(answer.begin(), answer.end());
+
+	return answer;
+}
